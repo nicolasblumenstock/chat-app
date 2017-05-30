84c9d7f8f02a251d60101e015b7b8b85dd842a58
Nicolas Blumenstock <nicpblum@gmail.com>
2017-05-30 16:13:23 -0400

    now a mostly functioning chat app with user joins, removing users who leave, timestamps

--- index.js
+++ index.js
@@ -2,9 +2,79 @@
 
 var http = require("http");
 var fs = require("fs");
+
+// include socket.io which was installed by npm. not part of core.
+
+var socketio = require("socket.io");
+
+var usersArray = [];
+
+
+
+
+
 var server = http.createServer((req,res)=>{
 	console.log('someone connected via http')
-})
+	if(req.url == '/'){
+		fs.readFile('index.html', 'utf-8', (error,data)=>{
+			// console.log(data);
+			// console.log(error);
+			if(error){
+				res.writeHead(500,{'content-type':'text/html'});
+				res.end('Internal Server Error');
+			}else{
+				res.writeHead(200,{'content-type':'text/html'});
+				res.end(data);
+			}
+		});
+	}else if(req.url == '/styles.css'){
+		fs.readFile('styles.css', 'utf-8', (error,data)=>{
+			// console.log(data);
+			// console.log(error);
+			if(error){
+				res.writeHead(500,{'content-type':'text/html'});
+				res.end('Internal Server Error');
+			}else{
+				res.writeHead(200,{'content-type':'text/css'});
+				res.end(data);
+			}
+		});
+	}
+
+});
+
+var io = socketio.listen(server);
+// handle socket connections
+io.sockets.on('connect', (socket)=>{
+	console.log('someone connected via socket');
+	socket.on('nameToServer', (data)=>{
+		// console.log(name + ' just joined')
+		var clientInfo = new Object();
+		clientInfo.name = data;
+		clientInfo.id = socket.id;
+		usersArray.push(clientInfo);
+		console.log(usersArray)
+		io.sockets.emit('usersArray', usersArray);
+	});
+	socket.on('sendMessage', ()=>{
+		console.log('someone clicked the button');
+	});
+	socket.on('messageToServer', (messageObject)=>{
+		io.sockets.emit('messageToClient',`<b>${messageObject.name}</b> (${messageObject.day}):&nbsp;&nbsp;${messageObject.newMessage}`);
+	});
+	socket.on('disconnect', (data)=>{
+		// console.log(data);
+		for (let i = 0; i < usersArray.length; i++){
+			var currentUser = usersArray[i];
+			if (currentUser.id == socket.id){
+				usersArray.pop(currentUser);
+				break;
+			}
+		}
+		console.log(usersArray)
+		io.sockets.emit('userDisconnect',usersArray);
+	})
+});
 
 server.listen(8080);
 console.log("listening on 8080");
